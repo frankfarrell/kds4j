2db8d4ba53c247e65cec914a55fbbd6f836e6e22
frankfarrell <ofearghp@gmail.com>
2018-03-29 17:29:40 +0100

    test: Further unit tests for queue state at queue initialisation

--- lib/src/test/java/com/github/frankfarrell/kds4j/KineticPriorityQueueTest.java
+++ lib/src/test/java/com/github/frankfarrell/kds4j/KineticPriorityQueueTest.java
@@ -54,7 +54,6 @@ public class KineticPriorityQueueTest {
 
     @Test
     public void itHasTheCorrectOrderingAtTheInitialTime() {
-        KineticPriorityQueue<String> queueUnderTest = new KineticPriorityQueue<String>(9.0);
 
         /*
         Priority will be the follow
@@ -64,14 +63,49 @@ public class KineticPriorityQueueTest {
         8  -> B, C, A
         10 -> C, B, A
          */
+        QueueElement<String> first = new QueueElement<>("A", x -> 8 - x);
+        QueueElement<String> second = new QueueElement<>("B", x -> x / 2 + 5);
+        QueueElement<String> third = new QueueElement<>("C", x -> (x * x) / 2 - 4 * x);
 
-        queueUnderTest.add(new QueueElement<>("A", x -> 8 - x));
-        queueUnderTest.add(new QueueElement<>("B", x -> x / 2 + 5));
-        queueUnderTest.add(new QueueElement<>("C", x -> (x * x) / 2 - 4 * x));
+        KineticPriorityQueue<String> queueUnderTestAtZero = new KineticPriorityQueue<String>(0.0);
 
-        assertThat(queueUnderTest.poll().element).isEqualTo("B");
-        assertThat(queueUnderTest.poll().element).isEqualTo("C");
-        assertThat(queueUnderTest.poll().element).isEqualTo("A");
+        queueUnderTestAtZero.add(first);
+        queueUnderTestAtZero.add(second);
+        queueUnderTestAtZero.add(third);
+
+        assertThat(queueUnderTestAtZero.poll().element).isEqualTo("A");
+        assertThat(queueUnderTestAtZero.poll().element).isEqualTo("B");
+        assertThat(queueUnderTestAtZero.poll().element).isEqualTo("C");
+
+        KineticPriorityQueue<String> queueUnderTestAt3 = new KineticPriorityQueue<String>(3.0);
+
+        queueUnderTestAt3.add(first);
+        queueUnderTestAt3.add(second);
+        queueUnderTestAt3.add(third);
+
+        assertThat(queueUnderTestAt3.poll().element).isEqualTo("B");
+        assertThat(queueUnderTestAt3.poll().element).isEqualTo("A");
+        assertThat(queueUnderTestAt3.poll().element).isEqualTo("C");
+
+        KineticPriorityQueue<String> queueUnderTestAt9 = new KineticPriorityQueue<String>(9.0);
+
+        queueUnderTestAt9.add(first);
+        queueUnderTestAt9.add(second);
+        queueUnderTestAt9.add(third);
+
+        assertThat(queueUnderTestAt9.poll().element).isEqualTo("B");
+        assertThat(queueUnderTestAt9.poll().element).isEqualTo("C");
+        assertThat(queueUnderTestAt9.poll().element).isEqualTo("A");
+
+        KineticPriorityQueue<String> queueUnderTestAt10 = new KineticPriorityQueue<String>(10.0);
+
+        queueUnderTestAt10.add(first);
+        queueUnderTestAt10.add(second);
+        queueUnderTestAt10.add(third);
+
+        assertThat(queueUnderTestAt10.poll().element).isEqualTo("C");
+        assertThat(queueUnderTestAt10.poll().element).isEqualTo("B");
+        assertThat(queueUnderTestAt10.poll().element).isEqualTo("A");
 
     }
     @Test
